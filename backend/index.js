97bf4af19e17cac669a1c33f87475b8588eb063f
ReyhanDiansa <reyhandiansa@gmail.com>
2023-01-25 08:00:29 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://diansa:123@cluster0.9u0iiws.mongodb.net/crud_db", { useNewUrlParser: true });
+
+const User = mongoose.model("Users", {
+  name: String,
+  email: String,
+  gender: String,
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/users", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+  };
+  const user = new User(data);
+  await user.save();
+  res.send(user);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+});
+
+app.put("/users/:id", async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body);
+  res.send("data has been update");
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const user = await User.findByIdAndRemove(req.params.id);
+  res.send(user);
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
